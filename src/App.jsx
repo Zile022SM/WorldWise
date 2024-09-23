6ea46305b9037e5c86593c72c03f09d039cd5b68
Zile022SM <zile.kuzminac85@gmail.com>
2024-09-23 21:19:27 +0200

    Display cities

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
-import AppNav from './components/AppNav';
 import './App.css';
 import Login from './pages/Login';
+import AppLayout from './pages/AppLayout';
+import CityList from './components/CityList';
 
 function App() {
   
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const BASE_URL = 'http://localhost:8000';
+
+  useEffect(function(){
+    async function fetchCities() { 
+      
+      try{
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+        setIsLoading(false);  
+      }catch(error){
+        alert(error);
+      }finally{
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }
+  ,[]);
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route index element={<Homepage />} />
           <Route path="/product" element={<Product />} />
           <Route path="/pricing" element={<Pricing />} />
-          <Route path="/app" element={<AppNav />} />
+          <Route path="/app" element={<AppLayout />} >
+            <Route index element={<CityList cities={cities} isLoading={isLoading}/>} />
+            <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+            <Route path="countries" element={<p>Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<PageNotFound/>} />
         </Routes>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './CityList.module.css';
+import Spinner from './Spinner';
+import CityItem from './CityItem';
+
+
+function CityList({ cities,isLoading}) {
+ 
+
+    if(isLoading) return <Spinner />;
+
+    return (
+        <ul className={styles.cityList}>
+            {cities.map((city) => <CityItem key={city.id} city={city} />)}
+        </ul>
+    );
+}
+
+export default CityList;
